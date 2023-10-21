f174176963e11978d45499d23279af8284f833cc
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-10-21 23:52:29 +0600

    implemented sorted system

---
+++ src/Pages/Sorting/Sorting/Sorting.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import Visuals from "../Visuals/Visuals";
+
+const Sorting = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Visuals />
+    </div>
+  );
+};
+
+export default Sorting;
